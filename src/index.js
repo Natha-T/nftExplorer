a3037b98c97100327dddcbc5ca6c475e2480479e
snpxx <lenaat220@gmail.com>
2022-05-31 16:47:17 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+ 
   <React.StrictMode>
-    <App />
+ <MoralisProvider appId='5IiNn1z4RdwrIuHcIeK3OmBoHqwhh3uC7IGuqaDl' serverUrl='https://mvn3qz8awnaz.usemoralis.com:2053/server'>
+
+    <App /> 
+     </MoralisProvider>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+
+    "./index.html",
+    "./src/App.js",
+    "./src/index.js",
+    "./src/component/Footer.js",
+    "./src/component/Bienvenue.js",
+    
+    "./src/component/Produit.js",
+    "./src/component/Propos.js",
+    "./src/component/Navbar.js",
+    "./src/component/Presentation.js",
+
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
